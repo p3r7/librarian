f60bae603f9c574b855b68594b23ab689e19ef7c
p3r7 <11557146+p3r7@users.noreply.github.com>
2024-09-17 21:27:46 +0200

    fix device assignment & nb for static hw registering

--- lib/hwutils.lua
+++ lib/hwutils.lua
@@ -7,6 +7,8 @@ local hwutils = {}
 -- deps
 
 local paramutils = include('librarian/lib/paramutils')
+local midiutil = include('librarian/lib/midiutil')
+local nbutils = include('librarian/lib/nbutils')
 
 
 -- ------------------------------------------------------------------------
@@ -27,6 +29,13 @@ function hwutils.hw_from_static(t, id, count, midi_device, ch, nb)
     hw.ch = 1
   end
 
+  -- had default hw device name, typically USB midi devices
+  if hw.midi_device and midi_device == midiutil.MIDI_DEV_ALL then
+    -- pass
+  else
+    hw.midi_device = midi_device
+  end
+
   hw.id = id
   hw.fqid = hw.shorthand.."_"..id
   if count > 1 then
@@ -35,6 +44,9 @@ function hwutils.hw_from_static(t, id, count, midi_device, ch, nb)
 
   if hw.plays_notes then
     hw.nb = true
+    if nb ~= nil and not nb then
+      hw.nb = false
+    end
   end
 
   hw.param_props = {}
@@ -50,6 +62,10 @@ function hwutils.hw_from_static(t, id, count, midi_device, ch, nb)
 
   function hw:register_params()
     paramutils.add_params(hw, hw.param_props, hw.param_list)
+  end
+
+  function hw:register_nb_players()
+    nbutils.register_player(self, "global")
   end
 
   return hw
